f794ae96d3c4dc5fce040f9da6e722cf4318c4a9
Szabolcs Szasz <szasz.szabolcs1995@gmail.com>
2026-06-25 10:19:47 +0200

    emta pixel

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,4 +1,5 @@
 import { Nunito, Caveat, Inter } from "next/font/google";
+import Script from "next/script";
 import "./globals.css";
 
 const nunito = Nunito({
@@ -55,7 +56,32 @@ export default function RootLayout({ children }) {
       lang="hu"
       className={`${nunito.variable} ${caveat.variable} ${inter.variable} antialiased`}
     >
-      <body className="min-h-screen flex flex-col">{children}</body>
+      <body className="min-h-screen flex flex-col">
+        <noscript>
+          <img
+            height="1"
+            width="1"
+            style={{ display: "none" }}
+            src="https://www.facebook.com/tr?id=982057567970800&ev=PageView&noscript=1"
+            alt=""
+          />
+        </noscript>
+        {children}
+      </body>
+      <Script id="meta-pixel" strategy="afterInteractive">
+        {`
+          !function(f,b,e,v,n,t,s)
+          {if(f.fbq)return;n=f.fbq=function(){n.callMethod?
+          n.callMethod.apply(n,arguments):n.queue.push(arguments)};
+          if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';
+          n.queue=[];t=b.createElement(e);t.async=!0;
+          t.src=v;s=b.getElementsByTagName(e)[0];
+          s.parentNode.insertBefore(t,s)}(window, document,'script',
+          'https://connect.facebook.net/en_US/fbevents.js');
+          fbq('init', '982057567970800');
+          fbq('track', 'PageView');
+        `}
+      </Script>
     </html>
   );
 }
